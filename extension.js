e62ff0d22a29ceb0867870fd72cdd12b4824c443
Zeng <luozengcn@gmail.com>
2025-02-05 15:43:26 +0800

    support drag to change tile order

--- extension.js
+++ extension.js
@@ -42,6 +42,10 @@ export default class DragnTileExtension extends Extension {
 
         this._tileTip = null;
         this._showingTip = false;
+        this._quadant = -1;
+
+        this._source = undefined;
+        this._target = undefined;
     }
 
     disable() {
@@ -49,7 +53,6 @@ export default class DragnTileExtension extends Extension {
     }
 
     _onDragDrop(event) {
-        console.error('DragnTileExtension._onDragDrop');
         if (this._showingTip && this._tileTip) {
             this._tileTip.hide();
             this._showingTip = false;
@@ -58,114 +61,166 @@ export default class DragnTileExtension extends Extension {
         // let id = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
         // });
         // GLib.Source.set_name_by_id(id, '[gnome-shell] extension.dragNtile');
-        let source = event.dropActor;
-        let target = event.targetActor;
 
-        while (target) {
-            if (target._delegate && target._delegate._getCaption && source._delegate && source._delegate._getCaption) {
-                console.error('DragnTileExtension.upon-app ', source._delegate._getCaption(), ' on ', target._delegate._getCaption());
+        if (this._target instanceof WindowPreview && this._source instanceof WindowPreview) {
+            console.debug('DragnTileExtension.upon-app ', this._source.get_name(), ' on ', this._target.get_name());
 
-                let monitor = target.metaWindow.get_monitor();
-                let workspace = target.metaWindow.get_workspace();
-                let monitorWorkArea = workspace.get_work_area_for_monitor(monitor);
+            let monitor = this._target.metaWindow.get_monitor();
+            let workspace = this._target.metaWindow.get_workspace();
+            let monitorWorkArea = workspace.get_work_area_for_monitor(monitor);
 
-                target._activate();
-                target.metaWindow.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
-                target.metaWindow.unmaximize(Meta.MaximizeFlags.VERTICAL);
-                target.metaWindow.move_resize_frame(false, monitorWorkArea.width/2, 0, monitorWorkArea.width/2, monitorWorkArea.height);
+            if (this._quadrant == 0 || this._quadrant == 3) {
+                // source | target
+                this._target._activate();
+                this._target.metaWindow.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
+                this._target.metaWindow.unmaximize(Meta.MaximizeFlags.VERTICAL);
+                this._target.metaWindow.move_resize_frame(false, monitorWorkArea.width/2, 0, monitorWorkArea.width/2, monitorWorkArea.height);
 
-                source._activate();
-                source.metaWindow.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
-                source.metaWindow.unmaximize(Meta.MaximizeFlags.VERTICAL);
-                source.metaWindow.move_resize_frame(false, 0, 0, monitorWorkArea.width/2, monitorWorkArea.height);
+                this._source._activate();
+                this._source.metaWindow.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
+                this._source.metaWindow.unmaximize(Meta.MaximizeFlags.VERTICAL);
+                this._source.metaWindow.move_resize_frame(false, 0, 0, monitorWorkArea.width/2, monitorWorkArea.height);
+            } else if (this._quadrant == 1 || this._quadrant == 2) {
+                // target | source
+                this._target._activate();
+                this._target.metaWindow.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
+                this._target.metaWindow.unmaximize(Meta.MaximizeFlags.VERTICAL);
+                this._target.metaWindow.move_resize_frame(false, 0, 0, monitorWorkArea.width/2, monitorWorkArea.height);
 
-                break;
+                this._source._activate();
+                this._source.metaWindow.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
+                this._source.metaWindow.unmaximize(Meta.MaximizeFlags.VERTICAL);
+                this._source.metaWindow.move_resize_frame(false, monitorWorkArea.width/2, 0, monitorWorkArea.width/2, monitorWorkArea.height);
             }
-            target = target.get_parent();
+
+            this._quadrant = -1;
+            // release window resource
+            this._source = undefined;
+            this._target = undefined;
         }
-        // let the workspace finish the dragdrop procedure
+
         return DND.DragDropResult.CONTINUE;
     }
 
     _onDragMotion(event) {
-        console.error('DragnTileExtension._onDragMotion x ', event.x, ' y ', event.y);
+        console.debug('DragnTileExtension._onDragMotion x ', event.x, ' y ', event.y, event.targetActor.get_name());
         let source = event.source;
 
-        let showTip = false;
-        source._workspace._windows.forEach((windowPreview, actor) => {
-            if (windowPreview instanceof WindowPreview && windowPreview !== source) {
+        // if drag point intersects any WindowPreview
+        // TODO: maybe peek actors at point?
+        let quadrant = -1;
+        for (const window of source._workspace._windows) {
+            if (window instanceof WindowPreview && window !== source) {
                 const topleft = new Graphene.Point3D({x: 0, y: 0});
-                const rightbottom = new Graphene.Point3D({x: windowPreview.get_width(), y: windowPreview.get_height()});
+                const rightbottom = new Graphene.Point3D({x: window.get_width(), y: window.get_height()});
                 // translate to screen coordinate
-                let {x: left, y: top} = windowPreview.apply_transform_to_point(topleft);
-                let {x: right, y: bottom} = windowPreview.apply_transform_to_point(rightbottom);
+                let {x: left, y: top} = window.apply_transform_to_point(topleft);
+                let {x: right, y: bottom} = window.apply_transform_to_point(rightbottom);
 
-                console.error('DragnTileExtension.drag: source', source.get_name(), event.x, event.y, 'window', windowPreview.get_name(), left, top, right, bottom);
+                quadrant = this._getQuadrant({x1: left, y1: top, x2: right, y2: bottom}, {x: event.x, y: event.y});
+                console.debug('DragnTileExtension.drag: source', source.get_name(), event.x, event.y, 'window', window.get_name(), left, top, right, bottom, quadrant);
+                if (quadrant != -1) {
+                    this._source = source;
+                    this._target = window;
 
+                    break;
+                }
+            }
 
-                if (left < event.x && event.x < right && top < event.y && event.y < bottom) {
-                    showTip = true;
-                    const dstBound = new Mtk.Rectangle({
+        }
+
+        console.error('DragnTileExtension.drag: quadrant', this._quadrant, quadrant);
+        if (this._quadrant != quadrant) {
+            this._quadrant = quadrant;
+            if (this._quadrant != -1) {
+                let target = event.targetActor;
+                let dstBound = undefined;
+                const topleft = new Graphene.Point3D({x: 0, y: 0});
+                const rightbottom = new Graphene.Point3D({x: target.get_width(), y: target.get_height()});
+                // translate to screen coordinate
+                let {x: left, y: top} = target.apply_transform_to_point(topleft);
+                let {x: right, y: bottom} = target.apply_transform_to_point(rightbottom);
+
+                if (this._quadrant == 0 || this._quadrant == 3) {
+                    dstBound = new Mtk.Rectangle({
                         x: left,
                         y: top,
                         width: (right - left) / 2,
                         height: (bottom - top)});
-                    let target = event.targetActor;
-                    while (target) {
-                        if (target instanceof WindowPreview && target !== source) {
-                            console.error('DragnTileExtension.target: ', target.get_name());
-                            if (!this._showingTip) {
-                                this._tileTip = new St.Widget({
-                                    name: 'DragnTileTip',
-                                    style_class: 'tile-preview',
-                                    x: target.get_x(),
-                                    y: target.get_y(),
-                                    width: target.get_width(),
-                                    height: target.get_height(),
-                                    opacity: 0,
-                                });
-                                console.error('DragnTileExtension.add_child parent:', target.get_name(), ' child:', this._tileTip.get_name());
-
-                                Main.uiGroup.add_child(this._tileTip);
-                                Main.uiGroup.set_child_above_sibling(this._tileTip, null);
-                                // put dnd on top of tileTip
-                                if (source._draggable && source._draggable._dragActor) {
-                                    Main.uiGroup.set_child_above_sibling(source._draggable._dragActor, null);
-                                }
-
-                                this._tileTip.show();
-                                this._tileTip.ease({
-                                    x: dstBound.x,
-                                    y: dstBound.y,
-                                    width: dstBound.width,
-                                    height: dstBound.height,
-                                    opacity: 255,
-                                    duration: WINDOW_ANIMATION_TIME,
-                                    mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-                                });
-                                console.error('DragnTileExtension.ease x:', dstBound.x, ' y:', dstBound.y,
-                                    ' w:', dstBound.width, ' h:', dstBound.height);
-                                // console.error('DragnTileExtension.ease');
-                                // console.error('DragnTileExtension.ease');
-                                // console.error('DragnTileExtension.ease');
-                                // console.error('DragnTileExtension.ease');
-
-                                this._showingTip = true;
-                                break;
-                            }
-                        }
-                        target = target.get_parent();
-                    }
                 }
+                if (this._quadrant == 1 || this._quadrant == 2) {
+                    dstBound = new Mtk.Rectangle({
+                        x: left + (right - left) / 2,
+                        y: top,
+                        width: (right - left) / 2,
+                        height: (bottom - top)});
+                }
+                console.debug('DragnTileExtension.drag: dst', dstBound.x, dstBound.y, dstBound.width, dstBound.height);
 
+                while (target) {
+                    if (target instanceof WindowPreview && target !== source) {
+                        console.error('DragnTileExtension.target: ', target.get_name());
+                        if (this._tileTip) {
+                            this._tileTip.hide();
+                        }
+                        this._tileTip = new St.Widget({
+                            name: 'DragnTileTip',
+                            style_class: 'tile-preview',
+                            x: left,
+                            y: top,
+                            width: right - left,
+                            height: bottom - top,
+                            opacity: 0,
+                        });
+
+                        Main.uiGroup.add_child(this._tileTip);
+                        Main.uiGroup.set_child_above_sibling(this._tileTip, null);
+                        // put dnd on top of tileTip
+                        if (source._draggable && source._draggable._dragActor) {
+                            Main.uiGroup.set_child_above_sibling(source._draggable._dragActor, null);
+                        }
+
+                        this._tileTip.show();
+                        this._tileTip.ease({
+                            x: dstBound.x,
+                            y: dstBound.y,
+                            width: dstBound.width,
+                            height: dstBound.height,
+                            opacity: 255,
+                            duration: WINDOW_ANIMATION_TIME,
+                            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+                        });
+                        console.debug('DragnTileExtension.ease x:', dstBound.x, ' y:', dstBound.y,
+                            ' w:', dstBound.width, ' h:', dstBound.height);
+
+                        this._showingTip = true;
+                        break;
+                    }
+                    target = target.get_parent();
+                }
             }
-        });
-        if (!showTip && this._showingTip) {
+        }
+
+        if (this._quadrant == -1 && this._showingTip) {
             this._tileTip.hide();
             this._showingTip = false;
         }
 
         // always listen dragmotion event
         return DND.DragDropResult.CONTINUE;
+    }
+
+    _getQuadrant(rect, point) {
+        if (point.x > rect.x1 && point.x < (rect.x1 + rect.x2) / 2 && point.y > rect.y1 && point.y < (rect.y1 + rect.y2) / 2) {
+            return 0;
+        } else if (point.x > (rect.x1 + rect.x2) / 2 && point.x < rect.x2 && point.y > rect.y1 && point.y < (rect.y1 + rect.y2) / 2) {
+            return 1;
+        } else if (point.x > (rect.x1 + rect.x2) / 2 && point.x < rect.x2 && point.y > (rect.y1 + rect.y2) / 2 && point.y < rect.y2) {
+            return 2;
+        } else if (point.x > rect.x1 && point.x < (rect.x1 + rect.x2) / 2 && point.y > (rect.y1 + rect.y2) / 2 && point.y < rect.y2) {
+            return 3
+        } else {
+            return -1;
+        }
     }
 }
